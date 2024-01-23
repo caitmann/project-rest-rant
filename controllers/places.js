26309435f640fc104fc99ad414d3920dc28e2bfa
caitmann <caitlynleemann@gmail.com>
2024-01-23 18:58:04 -0500

    adding places index page view

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,21 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'Harlem Java House',
+        city: 'Harlem',
+        state: 'GA',
+        cuisines: 'coffee, bakery',
+        pic: 'https://unsplash.com/photos/flat-lay-photography-of-coffee-latte-ground-coffee-and-coffee-beans-Y3AqmbmtLQI'
+    }, {
+        name: 'Hells Kitchen',
+        city: 'Las Vegas',
+        state: 'NV',
+        cuisines: 'British, American',
+        pic: 'https://unsplash.com/photos/red-flame-iZwQbx4T8bQ'
+    }
+    ]
+    res.render('places/index', {places})
 })
 
 module.exports = router
