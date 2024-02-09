19755ddba2ef7ab92a62790467735e7513f2e501
caitmann <caitlynleemann@gmail.com>
2024-02-09 17:06:39 -0500

    creating showEstablished method for place schema

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,25 +6,33 @@ function show (data) {
         <Def>
             <main>
                 <h1>{data.place.name}</h1>
-                <img src={data.place.pic} alt={data.place.name} />
-                <p>{data.place.cuisines}</p>
-                <p>{data.place.city}, {data.place.state}</p>
-                <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
-                    Edit
-                </a>
-                <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-                    <buttom type='submit' className='btn btn-danger'>
-                        Delete
-                    </buttom>
-                </form>
-                <section className='ratingSection'>
-                    <h2>Ratings</h2>
-                    <p>Currently Unrated</p>
-                </section>
-                <section className='commentSection'>
-                    <h2>Comments</h2>
-                    <p>No comments yet!</p>
-                </section>
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>Located in {data.place.city}, {data.place.state}</h3>
+                    </div>
+                    <div className='col-sm-6'>
+                        <h2>Description</h2>
+                        <h3>{data.place.showEstablished()}</h3>
+                        <h4>Serving {data.place.cuisines}</h4>
+                        <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                            Edit
+                        </a>
+                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                            <buttom type='submit' className='btn btn-danger'>
+                                Delete
+                            </buttom>
+                        </form>
+                    </div>
+                    <section className='ratingSection'>
+                        <h2>Ratings</h2>
+                        <p>Currently Unrated</p>
+                    </section>
+                    <section className='commentSection'>
+                        <h2>Comments</h2>
+                        <p>No comments yet!</p>
+                    </section>
+                </div>
             </main>
         </Def>
     )
